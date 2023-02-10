7d8e6955efecacb95fdbdafe0edb67eeae636486
Moanisha <moanisha94@gmail.com>
2023-02-10 17:11:06 -0500

    removed other code

--- routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-const GetUserProfileController = require("../controllers/client/GetUserProfile");
-
-const router = require("express").Router();
-
-//user profile
-router.get("/profile/:userId", GetUserProfileController);
-
-module.exports = router;
